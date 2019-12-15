3161f66e05a1b204b1d921a5e8b2787536c27cdc
wenq97 <wenq97@uw.edu>
2019-12-14 21:43:28 -0800

    Change domain redirection

--- src/index.js
+++ src/index.js
@@ -35,22 +35,9 @@ ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Switch>
-        <Route path="/index" render={props => <Index {...props} />} />
-        <Route
-          path="/nucleo-icons"
-          render={props => <NucleoIcons {...props} />}
-        />
-        <Route
-          path="/landing-page"
-          render={props => <LandingPage {...props} />}
-        />
-        <Route
-          path="/profile-page"
-          render={props => <ProfilePage {...props} />}
-        />
-        <Route path="/login-page" render={props => <LoginPage {...props} />} />
-        <Redirect to="/index" />
-        <Redirect from="/" to="/index" />
+        <Route path="/personal-website-react" render={props => <Index {...props} />} />
+        <Redirect to="/personal-website-react" />
+        <Redirect from="/" to="/personal-website-react" />
       </Switch>
     </Switch>
   </BrowserRouter>,
