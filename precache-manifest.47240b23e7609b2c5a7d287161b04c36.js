cfb65078b3cc00d2fccfba24db9ba2717873630f
wenq97 <wenq97@uw.edu>
2019-12-14 21:10:54 -0800

    Updates

---
+++ precache-manifest.47240b23e7609b2c5a7d287161b04c36.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c77410fb268858ad2cb120a41e06a40",
+    "url": "/personal-website-react/index.html"
+  },
+  {
+    "revision": "c93a0789e0bcde55d1b0",
+    "url": "/personal-website-react/static/css/main.fd79427c.chunk.css"
+  },
+  {
+    "revision": "e46b203fc9b67a610f1b",
+    "url": "/personal-website-react/static/js/2.a7750de8.chunk.js"
+  },
+  {
+    "revision": "c93a0789e0bcde55d1b0",
+    "url": "/personal-website-react/static/js/main.7bfccc12.chunk.js"
+  },
+  {
+    "revision": "a2191c816f220f664f1c",
+    "url": "/personal-website-react/static/js/runtime~main.7e5eeb1b.js"
+  },
+  {
+    "revision": "86c711e14cec4abbf7b7af9a3f741016",
+    "url": "/personal-website-react/static/media/WENQIU-Resume.86c711e1.pdf"
+  },
+  {
+    "revision": "76839d22592cd333e14819fc2cb7c0aa",
+    "url": "/personal-website-react/static/media/adapted-toy-inventory.76839d22.png"
+  },
+  {
+    "revision": "7257a1a94be550133f3233fd792ee724",
+    "url": "/personal-website-react/static/media/avatar.7257a1a9.jpg"
+  },
+  {
+    "revision": "6b1983a26a32647c4832456dc9f5dcd3",
+    "url": "/personal-website-react/static/media/bg1.6b1983a2.jpg"
+  },
+  {
+    "revision": "e643a6cf77b4635dda856f10fa9b7db4",
+    "url": "/personal-website-react/static/media/bg11.e643a6cf.jpg"
+  },
+  {
+    "revision": "f807b37b1736e7709918ea35ef80ca7d",
+    "url": "/personal-website-react/static/media/bg3.f807b37b.jpg"
+  },
+  {
+    "revision": "056eb0586182a8d28273ad11c028e912",
+    "url": "/personal-website-react/static/media/bg5.056eb058.jpg"
+  },
+  {
+    "revision": "d7d1aaf71d275992031bd75cdb6ec65c",
+    "url": "/personal-website-react/static/media/bg6.d7d1aaf7.jpg"
+  },
+  {
+    "revision": "607a4a36d058e55773625ba23518e291",
+    "url": "/personal-website-react/static/media/bg7.607a4a36.jpg"
+  },
+  {
+    "revision": "6737ce3b2577a1c3977e4d479511f642",
+    "url": "/personal-website-react/static/media/bg8.6737ce3b.jpg"
+  },
+  {
+    "revision": "f7cb2d390498c2969b2b1050f3e024ce",
+    "url": "/personal-website-react/static/media/creative-tim-white-slim2.f7cb2d39.png"
+  },
+  {
+    "revision": "1d4e7abca0bd3db3c39f435efeecff57",
+    "url": "/personal-website-react/static/media/css-lint.1d4e7abc.png"
+  },
+  {
+    "revision": "089c4f4e55f158a69d0364c6b6339f37",
+    "url": "/personal-website-react/static/media/eva.089c4f4e.jpg"
+  },
+  {
+    "revision": "ffcc99c42164b65da7af6307b66379cf",
+    "url": "/personal-website-react/static/media/header.ffcc99c4.jpg"
+  },
+  {
+    "revision": "a76c7b4ce725bb07b1572699a5a78c62",
+    "url": "/personal-website-react/static/media/hero-image-1.a76c7b4c.png"
+  },
+  {
+    "revision": "9616730d099782a3ea6c12c70aa181af",
+    "url": "/personal-website-react/static/media/hero-image-2.9616730d.png"
+  },
+  {
+    "revision": "02cd0e2d2b27926d1ed5c31a00dd26fa",
+    "url": "/personal-website-react/static/media/hero-image-3.02cd0e2d.png"
+  },
+  {
+    "revision": "f71bfc5fd75fdb0a2350bb4be06c7db2",
+    "url": "/personal-website-react/static/media/homebg.f71bfc5f.jpg"
+  },
+  {
+    "revision": "300494e9857b8d865ead79a62b09dd88",
+    "url": "/personal-website-react/static/media/invision-white-slim.300494e9.png"
+  },
+  {
+    "revision": "138381e51c42397348a82b448b5fb61b",
+    "url": "/personal-website-react/static/media/julie.138381e5.jpg"
+  },
+  {
+    "revision": "3881d619aa3ffcd16f920c177e175357",
+    "url": "/personal-website-react/static/media/kickstarter.3881d619.png"
+  },
+  {
+    "revision": "f8fba45478e18757e67c37b6644a3cec",
+    "url": "/personal-website-react/static/media/landing.f8fba454.jpg"
+  },
+  {
+    "revision": "58ae5b4586b4bfc290a2efdfbdb90530",
+    "url": "/personal-website-react/static/media/linked-list-playground.58ae5b45.png"
+  },
+  {
+    "revision": "b21019516fd574253e2354080dc1dd00",
+    "url": "/personal-website-react/static/media/login.b2101951.jpg"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/personal-website-react/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/personal-website-react/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/personal-website-react/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/personal-website-react/static/media/nucleo-outline.8ebec31f.woff2"
+  },
+  {
+    "revision": "5a2efb31cf5dd73437600a28d66cc974",
+    "url": "/personal-website-react/static/media/profile-pic.5a2efb31.jpg"
+  },
+  {
+    "revision": "225653f9f4dacef3e81bcc52f5ef547d",
+    "url": "/personal-website-react/static/media/profile.225653f9.jpg"
+  },
+  {
+    "revision": "f217f346c88e5fb2720ca7c28827ff72",
+    "url": "/personal-website-react/static/media/ryan.f217f346.jpg"
+  },
+  {
+    "revision": "697bf81cd531e960a892e3e4c525560e",
+    "url": "/personal-website-react/static/media/synthesis.697bf81c.png"
+  },
+  {
+    "revision": "7663dc658a8e2a9cbfdf2016a5e7c1a5",
+    "url": "/personal-website-react/static/media/word-master.7663dc65.png"
+  },
+  {
+    "revision": "5e882a69304cb75247760223646cd83f",
+    "url": "/personal-website-react/static/media/wq-logo.5e882a69.png"
+  }
+]);
